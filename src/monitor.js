58933ec4e2da86930e4cd5b00a1dac320148efa0
Rekai Musuka <musukarekai@gmail.com>
2019-06-02 15:40:25 -0500

    Basic Monitor written

---
+++ src/monitor.js
@@ -0,0 +1,61 @@
+const process = require('process');
+
+class Monitor {
+  constructor(interval) {
+    this.inProgress = false;
+    this.interval = interval || 360 // Default
+
+    const handleSuicide = signal => {
+      console.warn(`Save Sync has recieved ${signal}`);
+
+      // Confirm that nothign is being written to the files
+      // Zip the current files and move htem to a folder where 
+      //they can either be uploaded automatically or manually
+
+      const waitUntilReady = async () => {
+        while (this.inProgress) {
+          console.log("Waiting for backup to finish.");
+
+          await this.sleep(3);
+        }
+
+        process.exit(0);
+      }
+
+      waitUntilReady();
+    }
+
+    process.on('SIGINT', handleSuicide);
+    process.on('SIGTERM', handleSuicide)
+
+    this.run();
+  }
+
+  sleep(sec) {
+    return new Promise(resolve => setTimeout(resolve, sec * 1000))
+  }
+
+  async run() {
+    // We want to keep a Backup of what the save scurrently are before we start messing with them
+    // Before we start monitoring, we need to zip the current save files os the user can restore them.
+
+    try {
+      while (true) {
+        this.inProgress = true;
+
+        // Get known Directories
+        // Compare File contents. Compare checksums(?) to figure out wheether the files have changed
+
+        // Move files with different checksums(?) to where the files are backed up.
+        console.log("Test");
+        this.inProgress = false;
+        await this.sleep(this.interval);
+      }
+    } catch(e) { throw e; }
+
+
+
+  }
+}
+
+const monitor = new Monitor(3);
