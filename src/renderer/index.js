802adb97cea7118182f6240f4975330d6f85546a
Rekai Musuka <musukarekai@gmail.com>
2019-06-04 21:46:17 -0500

    Added titlebar with working minimize and close buttons

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+
+function createHeaderButtons() {
+  const close = document.querySelector('header div.window-icons i.fa-window-close');
+  const minimize = document.querySelector('header div.window-icons i.fa-window-minimize');
+  const remote = require('electron').remote;
+
+
+  close.onclick = () => {
+    console.log("Close button clicked.");
+    remote.app.quit();
+  }
+
+  minimize.onclick = () => {
+    console.log("Minimize button clicked.");
+    remote.BrowserWindow.getFocusedWindow().minimize()
+    
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  createHeaderButtons();
+}, false)
