6622b9761b6815eaa2411254977753bb37c35cd8
Rekai Musuka <musukarekai@gmail.com>
2019-06-06 11:47:05 -0500

    completed Settings Controller TOOD: Intergrate into UI

--- src/main/controllers/saveController.js
+++ src/main/controllers/saveController.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const fse = require('fs-extra');
 const SettingsController = require('./settingsController');
 const settings = new SettingsController();
 
@@ -7,50 +8,112 @@ class SaveController {
     const canLoadData = this._createSavePath();
 
     this.trackedSaves = {};
-    
-
     if (canLoadData) this.trackedSaves = this.getReferences();
-    console.log(this.trackedSaves);
   }
 
   add(path) {
     // Add a save
-    const clonePath = this.saveClonePath + settings.slash + this._generateUUID();
+    const cloneHome = this.saveClonePath + settings.slash + this._generateUUID();
+    const copyRoot = cloneHome + settings.slash + path.match(/\w+$/g)[0]
     this.trackedSaves[path] = {
       sourcePath: path,
-      clonePath: clonePath + settings.slash + path.match(/\w+$/g)[0],
+      cloneHome: cloneHome,
+      clonePath: copyRoot,
       lastUpdated: ~~(Date.now() / 1000)
     }
 
     // crawl and copy directories + their data structure to another directory
     // add a reference to the original, and it's duplicate in songs.json 
 
-    fs.mkdirSync(clonePath);
+    fs.mkdirSync(cloneHome); // Generate Location with UUID
+    fs.mkdirSync(copyRoot); //Make sure Root Folder is there. 
 
-    this._save(this.trackedSaves);
-  }
+    return new Promise((res, rej) => {
+      fse.copy(path, copyRoot, { recursive: true }, err => {
+        if (err) rej(err);
+        this._save(this.trackedSaves);
 
-  get(path) {
-    // Get a save
-
-    // get clone location of OG file path provided by user here.
-    // return the relevant information
+        console.log(path + "has been backed up.")
+        res();
+      })
+    })
   }
 
   getReferences() {
     return JSON.parse(fs.readFileSync(this.saveCloneFile, { encoding: 'utf8' }));
   }
 
-  update(path){
+  update(path, newPath){
     // Update info about save
+    this.trackedSaves[path].sourcePath = newPath;
+    this._save(this.trackedSaves);
 
-    // Similar to add but edites existing entries, rather than creates new ones
+  }
+
+  sync(path) {
+    return new Promise((res, rej) => {
+      const ref = this.trackedSaves[path];
+      this.trackedSaves[path].lastUpdated = ~~(Date.now() / 1000);
+
+      fse.remove(ref.clonePath, err => {
+        if (err) rej(err);
+
+        fse.copy(ref.sourcePath, ref.clonePath, { overwrite: true, recursive: true }, err => {
+          if (err) rej(err)
+          console.log(ref.sourcePath + " has been synced.");
+          res();
+
+          this._save(this.trackedSaves)
+        });
+      });
+    });
+  }
+
+  syncAll() {
+    return new Promise((res, rej) => {
+      for (let key in this.trackedSaves) {
+        this.trackedSaves[key].lastUpdated = ~~(Date.now() / 1000);
+        const ref = this.trackedSaves[key];
+
+        if (this.trackedSaves.hasOwnProperty(key)) {
+          fse.remove(ref.clonePath, err => {
+            if (err) rej(err);
+
+            fse.copy(ref.sourcePath, ref.clonePath, { overwrite: true, recursive: true}, err => {
+              if (err) rej(err); 
+              console.log(ref.sourcePath + " has been synced.");
+              this._save(this.trackedSaves);
+            });
+          });
+        }
+      }
+    });
   }
 
   remove(path) {
     // Delete save
-
     // Deletes the clone folder, and then its reference in saves.json
+    
+    return new Promise((res, rej) => {
+      fse.remove(this.trackedSaves[path].clonePath, err => {
+        if (err) rej(err);
+
+        console.log("Deleted backup of " + path);
+    
+        this.trackedSaves[path] = undefined;
+        this.trackedSaves = JSON.parse(JSON.stringify(this.trackedSaves));
+        res();
+      })
+    })
+
+
+    return new Promise((res, rej) => {
+      fse.rmdir(this.trackedSaves[path].clonePath, err => {
+        if (err) rej(err);
+
+      })
+
+    })
   }
 
   _save(newTrackedSaves) {
--- src/monitor.js
+++ src/monitor.js
@@ -1,4 +1,5 @@
 const process = require('process');
+const SaveController = require('./main/controllers/saveController');
 
 class Monitor {
   constructor(interval) {
@@ -7,10 +8,6 @@ class Monitor {
 
     const handleSuicide = signal => {
       console.warn(`Save Sync has recieved ${signal}`);
-
-      // Confirm that nothign is being written to the files
-      // Zip the current files and move htem to a folder where 
-      // they can either be uploaded automatically or manually
 
       const waitUntilReady = async () => {
         while (this.inProgress) {
@@ -34,22 +31,16 @@ class Monitor {
   }
 
   async run() {
-    // We want to keep a Backup of what the save scurrently are before we start messing with them
-    // Before we start monitoring, we need to zip the current save files os the user can restore them.
+    const saveController = new SaveController();
 
-    try {
-      while (true) {
-        this.inProgress = true;
+    while (true) {
+      this.inProgress = true;
 
-        // Get known Directories
-        // Compare File contents. Compare checksums(?) to figure out wheether the files have changed
-
-        // Move files with different checksums(?) to where the files are backed up.
-        console.log("Test");
-        this.inProgress = false;
-        await this.sleep(this.interval);
-      }
-    } catch(e) { throw e; }
+      await saveController.syncAll();
+      
+      this.inProgress = false;
+      await this.sleep(this.interval);
+    }
   }
 }
 
